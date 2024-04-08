b3f271820c59f58648c046d7169c2087b51f8d8f
ashutosh-353 <meherashutosh2004@gmail.com>
2024-04-08 21:31:47 +0530

    modified

--- hellojs.js
+++ hellojs.js
@@ -2,6 +2,23 @@ const wrapper = document.querySelector(".wrapper");
 
 const registerLink = document.querySelector(".register-link");
 
+function func() {
+  // var email1 = document.getElementById("user-email-1").value;
+  // var password1 = document.getElementById("user-pw-1").value;
+
+const email = document.getElementById("user-email").value;
+const password = document.getElementById('user-pw').value;
+
+if (email === 'meherashutosh2004@gmail.com' && password ==='1234') {
+  window.location.href="index2.html";
+  alert("Successful !");
+  
+} 
+else {
+  alert("Invalid Credentials");
+}
+}
+
 const loginLink = document.querySelector(".login-link");
 
 registerLink.addEventListener("click", function () {
